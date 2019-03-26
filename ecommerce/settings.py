dd0e90b2b1d5b15104a4ee93d72a5f50be8bed7e
sourabh <sourabh7singh@gmail.com>
2019-03-27 00:25:47 +0530

    production build

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
